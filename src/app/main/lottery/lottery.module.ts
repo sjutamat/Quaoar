65f024334e219110fc95f6a3e76f7bfcb824e308
sjutamat <ladywitch13@gmail.com>
2020-07-03 00:59:33 +0700

    UI Lottery

--- src/app/main/lottery/lottery.module.ts
+++ src/app/main/lottery/lottery.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { TranslateModule } from '@ngx-translate/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatTabsModule } from '@angular/material/tabs';
 
 import { FuseSharedModule } from '@fuse/shared.module';
+import { FuseDemoModule } from '@fuse/components/demo/demo.module';
+import { FuseHighlightModule } from '@fuse/components';
 
-import { LotteryComponent } from './lottery.component';
+import { LotteryComponent } from 'app/main/lottery/lottery.component';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
-const routes = [
+const routes: Routes = [
     {
         path     : 'lottery',
         component: LotteryComponent
@@ -20,15 +30,22 @@ const routes = [
     imports     : [
         RouterModule.forChild(routes),
 
-        TranslateModule,
+        MatButtonModule,
+        MatButtonToggleModule,
+        MatIconModule,
+        MatListModule,
+        MatMenuModule,
+        MatSelectModule,
+        MatSlideToggleModule,
+        MatTabsModule,
 
-        FuseSharedModule
-    ],
-    exports     : [
-        LotteryComponent
+        NgxChartsModule,
+
+        FuseSharedModule,
+        FuseDemoModule,
+        FuseHighlightModule,
     ]
 })
-
 export class LotteryModule
 {
 }
